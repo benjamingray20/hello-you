758569bc2c5570fa978246939ba4998d16c175cf
benjamingray20 <60855283+benjamingray20@users.noreply.github.com>
2020-05-20 21:29:50 -0400

    made some progress but getting an error in my index.jsx error on line 6

---
+++ components/aboutMe.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default () => (
+  <div className="page">
+    <div className="title">Ben Gray</div>
+    <div className="subtitle">Software Engineer with 9+ years of experience as
+    an IT Specialist. Skills in Javascript/HTML/CSS/Node/React. Dependable,
+    responsible and resourceful. Connects easily with colleagues, team members,
+    and leadership to achieve organizational goals. Continuous learner always
+      eager to know more. </div>
+  </div>
+)
